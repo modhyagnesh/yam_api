e30d621f0bbc283fbc17b19a312abd2aadf2477c
Yagnesh Modh <contact@thespecialcharacter.com>
2024-09-14 13:00:41 +0530

    commit code

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
+  jwt: {
+    secret: env("JWT_SECRET"),
+  },
 });
